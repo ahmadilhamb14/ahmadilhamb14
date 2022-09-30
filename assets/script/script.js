53d39057cad1250bc1bfbbf45f83376a310daf88
ahmadilhamb14 <56315963+ahmadilhamb14@users.noreply.github.com>
2022-09-30 13:37:52 +0800

    dfda

--- assets/script/script.js
+++
@@ -1,8 +0,0 @@
-var textarea = document.getElementById('message-text');
-var button = document.getElementById('submit');
-var hasil = document.getElementById('hasil');
-
-button.addEventListener('click', function () {
-  hasil.innerHTML = textarea.value;
-  console.log(textarea.value);
-});
